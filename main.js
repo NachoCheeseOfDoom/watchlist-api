c8c7cc43e041a02b977f46a278dc4da024f94d2e
NachoCheeseOfDoom <85528067+NachoCheeseOfDoom@users.noreply.github.com>
2024-08-07 22:47:39 -0600

    moved the fetch to the click event function

--- main.js
+++ main.js
@@ -5,14 +5,27 @@ const movieTitleEl = document.getElementById('movieTitle')
 const API_KEY = '19db8cf4'
 const url = `https://www.omdbapi.com/?apikey=${API_KEY}&`
 
-searchBtnEl.addEventListener('click', () => {
+searchBtnEl.addEventListener('click', async () => {
     const title = movieTitleEl.value
-    searchMovie(title)
-})
-
-async function searchMovie(movieTitle) {
-    const res = await fetch(`${url}t=${movieTitle}`)
+    const res = await fetch(`${url}t=${title}`)
     const data = await res.json()
+    if (!data.Error) {
+        displayMovieDetails(data)
+    } else {
+        alert('Movie not found.')
+    }
 
     console.log(data)
-}
+})
+
+// async function searchMovie(movieTitle) {
+//     const res = await fetch(`${url}t=${movieTitle}`)
+//     const data = await res.json()
+//     if (!data.Error) {
+//         displayMovieDetails(data)
+//     } else {
+//         alert('Movie not found.')
+//     }
+
+//     console.log(data)
+// }
